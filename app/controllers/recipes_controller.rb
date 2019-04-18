3fafee96313e4db631cd7d354728ce3c04c69c11
Marc Wright <marc@generalassemb.ly>
2019-04-18 11:22:52 +0300

    check boxes working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,5 +4,25 @@ class RecipesController < ApplicationController
     # render json: @recipes, include: :ingredients
   end
 
-  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create  
+    puts params
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :ingredient_ids => [])
+    end
+
+
 end
